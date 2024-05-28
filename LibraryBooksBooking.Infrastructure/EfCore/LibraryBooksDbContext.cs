0b3987fd7ef3977ba6a28366bd718955bf95e875
saidr <saidradwanmansour@hotmail.com>
2024-05-28 14:19:13 +0200

    DB trad constructor implemented

--- LibraryBooksBooking.Infrastructure/EfCore/LibraryBooksDbContext.cs
+++ LibraryBooksBooking.Infrastructure/EfCore/LibraryBooksDbContext.cs
@@ -1,28 +1,34 @@
 ﻿using LibraryBooksBooking.Core.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace LibraryBooksBooking.Infrastructure.EfCore;
-
-public class LibraryBooksDbContext(DbContextOptions<LibraryBooksDbContext> options) : DbContext(options)
+namespace LibraryBooksBooking.Infrastructure.EfCore
 {
-    public DbSet<Book> Books { get; set; }
-    public DbSet<Customer> Customers { get; set; }
-    public DbSet<Booking> Bookings { get; set; }
-    
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    public class LibraryBooksDbContext : DbContext
     {
-        modelBuilder.Entity<Book>().HasKey(x => x.Guid);
-        modelBuilder.Entity<Customer>().HasKey(x => x.Guid);
-        modelBuilder.Entity<Booking>().HasKey(x => x.Guid);
-        
-        modelBuilder.Entity<Booking>()
-            .HasOne(x => x.Customer)
-            .WithMany()
-            .HasForeignKey(x => x.CustomerGuid);
-        
-        modelBuilder.Entity<Booking>()
-            .HasOne(x => x.Book)
-            .WithMany()
-            .HasForeignKey(x => x.BookGuid);
+        public LibraryBooksDbContext(DbContextOptions<LibraryBooksDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasKey(x => x.Guid);
+            modelBuilder.Entity<Customer>().HasKey(x => x.Guid);
+            modelBuilder.Entity<Booking>().HasKey(x => x.Guid);
+
+            modelBuilder.Entity<Booking>()
+                .HasOne(x => x.Customer)
+                .WithMany()
+                .HasForeignKey(x => x.CustomerGuid);
+
+            modelBuilder.Entity<Booking>()
+                .HasOne(x => x.Book)
+                .WithMany()
+                .HasForeignKey(x => x.BookGuid);
+        }
     }
 }
