0c1ae7fc0883a9bcb4d207fd0011716f2b850e7f
Kjell Schoke <kjell.schoke@outlook.com>
2024-05-28 13:34:38 +0200

    Refactor to async

--- LibraryBooksBooking.Core/IRepositories/IRepository.cs
+++ LibraryBooksBooking.Core/IRepositories/IRepository.cs
@@ -2,9 +2,9 @@
 
 public interface IRepository<T>
 {
-    T Add(T entity);
-    IEnumerable<T> GetAll();
-    T Get(string id);
-    T Edit(T entity);
-    T Remove(T entity);
+    T AddAsync(T entity);
+    IEnumerable<T> GetAllAsync();
+    T GetAsync(string id);
+    T EditAsync(T entity);
+    T RemoveAsync(T entity);
 }
